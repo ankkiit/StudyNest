295852f97547d5b358bd30bc6af864e7bde1f294
Ankit9034 <ankitkashyap897v@gmail.com>
2025-10-24 17:28:41 +0530

    Added config file

--- backend/src/main/java/com/ankit/security/SecurityConfig.java
+++ backend/src/main/java/com/ankit/security/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of("http://localhost:5173")); // frontend origin
+		configuration.setAllowedOrigins(List.of("http://localhost:5173","https://study-nest-six.vercel.app")); // frontend origin
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setAllowCredentials(true);
